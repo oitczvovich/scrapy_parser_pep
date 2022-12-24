f595903c3e1af2de10c502f80742c5bf9793170c
Oitz <skalckii@yandex.ru>
2022-12-24 19:12:51 +0300

    v_0.1

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -5,7 +5,7 @@ BOT_NAME = 'pep_parse'
 SPIDER_MODULES = ['pep_parse.spiders']
 NEWSPIDER_MODULE = 'pep_parse.spiders'
 ROBOTSTXT_OBEY = True
-ITEM_PIPELINES = {'pep_parse.pipelines.PepParsePipeline': 300,}
+ITEM_PIPELINES = {'pep_parse.pipelines.PepParsePipeline': 300}
 FEEDS = {
     'results/pep_%(time)s.csv': {
         'format': 'csv',
