52798e53face001ded88ed6cebbf86f43063f4d5
Oitz <skalckii@yandex.ru>
2022-12-25 12:34:24 +0300

    v_0.1_fixed

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,28 +1,25 @@
 import csv
-from itemadapter import ItemAdapter
-from scrapy.exceptions import DropItem
 from pathlib import Path
 
-from .settings import NOW_TIME
+from itemadapter import ItemAdapter
 
+from .settings import NOW_TIME
 
 BASE_DIR = Path(__file__).parent.parent
 
 
 class PepParsePipeline:
     def open_spider(self, spider):
-        self.__status_dict = {}
+        self.__status_vocabulary = {}
 
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
         if adapter.get('status'):
             pep_status = adapter['status']
-            self.__status_dict[pep_status] = (
-                self.__status_dict.get(pep_status, 0) + 1
+            self.__status_vocabulary[pep_status] = (
+                self.__status_vocabulary.get(pep_status, 0) + 1
             )
             return item
-        else:
-            raise DropItem(f'Отсутствует статус в {item}')
 
     def close_spider(self, spider):
         RESULT_DIR = BASE_DIR / 'results'
@@ -33,7 +30,7 @@ class PepParsePipeline:
             ).writerows(
                 (
                     ("Статус", "Колличество"),
-                    *self.__status_dict.items(),
-                    ("Total", sum(self.__status_dict.values()))
+                    *self.__status_vocabulary.items(),
+                    ("Total", sum(self.__status_vocabulary.values()))
                 )
             )
